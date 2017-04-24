29f833f27591248a678a6ec6ecb11255ceed544b
PsychoTea <bensparkes8@gmail.com>
2017-04-24 21:41:05 +0100

    Created VS Project

---
+++ Oxide.Plugins/Oxide.Plugins/BanSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oxide;
+using Oxide.Plugins;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("BanSystem", "PsychoTea", "1.0.0")]
+
+    class BanSystem : RustPlugin
+    {
+        [ChatCommand("test")]
+        void testCommand(BasePlayer player, string command, string[] args)
+        {
+
+        }
+    }
+}
