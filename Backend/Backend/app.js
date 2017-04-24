950c5d2cbe56093c3a8e21160e291061d572eaa2
Kieran South <admin@spicee.xyz>
2017-04-24 22:21:54 +0100

    Add requires and config

--- Backend/Backend/app.js
+++ Backend/Backend/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Hello world');
+var config = require('./config');
+
+var express = require('express')();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var sanitise = require('express-mongo-sanitize');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(sanitise());
+
+express.listen(config.express.port);
+
+express.post(config.express.directory, (request, response) => {
+
+});
---
+++ Backend/Backend/config.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+	express: {
+		port: 1024,
+		directory: '/'
+	},
+	mongoose: {
+		host: 'localhost',
+		port: 27017,
+		auth: {
+			db: 'admin',
+			user: 'user',
+			pass: 'pass'
+		}
+	}
+}
